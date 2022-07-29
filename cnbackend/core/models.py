85a118cc26c071456b2eaff154a0477c218fcb9a
taha7900 <taha7900@gmail.com>
2022-07-29 19:59:38 +0430

    models in backend added

---
+++ cnbackend/core/models.py
@@ -0,0 +1,52 @@
+from email.policy import default
+from random import choices
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    ADMIN = 1
+    MANAGER = 2
+    USER = 0
+    USER_TYPE_CHOICES = [
+        (USER, 'user'),
+        (ADMIN, 'admin'),
+        (MANAGER, 'manager')
+    ]
+
+    full_name=models.CharField(max_length=30)
+    type=models.Choices(choices=USER_TYPE_CHOICES, default=USER)
+    strike=models.BooleanField(default=False)
+
+
+class Video(models.Model):
+    file=models.FileField(upload_to='/videos')
+    caption=models.CharField(max_length=50)
+    user=models.ForeignKey(to=User, on_delete=models.SET_NULL)
+    banned=models.BooleanField(default=False)
+
+
+class VideoTag(models.Model):
+    label=models.CharField(max_length=15)
+    video=models.ForeignKey(to=Video)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['label', 'video'], name='label_video_unique')
+        ]
+
+
+class Like(models.Model):
+    video=models.ForeignKey(to=Video, on_delete=models.CASCADE)
+    user=models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['video', 'user'], name='video_user_unique')
+        ]
+
+
+class Comment(models.Model):
+    text=models.TextField()
+    video=models.ForeignKey(to=Video, on_delete=models.CASCADE)
+    user=models.ForeignKey(to=User, on_delete=models.CASCADE)
