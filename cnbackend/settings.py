97f56388fd471d816283f66faf7c5be467465464
taha7900 <taha7900@gmail.com>
2022-08-01 00:12:24 +0430

    urls added

--- cnbackend/settings.py
+++ cnbackend/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'cnbackend.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
--- cnbackend/urls.py
+++ cnbackend/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
+from core.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', obtain_jwt_token),
     path('api/token/verify/', verify_jwt_token),
-    path('api/token/refresh/', refresh_jwt_token)
+    path('api/token/refresh/', refresh_jwt_token),
+
+    path('api/video/<int:id>', GetVideoData.as_view())
 ]
